7b4e0e5a64f01b3fe2e51264f14d780046c9d818
santhoshbalajikumar <santhoshbalaji04@gmail.com>
2017-06-27 12:23:01 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+int n1,n2,n3;
+scanf("%d%d%d",&n1,&n2,&n3);
+if(n1>=n2)
+{
+printf("n1 is greater");
+}
+elseif("n2>=n3")
+{
+printf("n2is greater");
+}
+else(n3>=n1)
+{
+printf("n3 is greater");
+}
+getch();
+}
+
+
