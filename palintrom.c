a8f9acf738e83c766a2560574da7419edb2bf5ac
santhoshbalajikumar <santhoshbalaji04@gmail.com>
2017-06-28 11:50:13 +0530

    Update palintrom.c

--- palintrom.c
+++ palintrom.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+void main()
+{
+  int a,b,c=0,r;
+  printf("Enter the number");
+  scanf("%d",&a);
+ while(a>0)
+ {
+   r=a%10;
+  c= c*10/r;
+  a=a/10;
+ }
+  if(b==c)
+  {
+    printf("it is the palindrome");
+  }
+  else
+  {
+    printf("not a palindrome");
+  }
+}
